b326f5f5773358784f422fd37a087ad12cb70465
SoulMurderer <abdelrhmanehab48@gmail.com>
2022-11-19 10:02:05 +0200

    was removed by mistake

--- droidducky.sh
+++ droidducky.sh
@@ -5,6 +5,7 @@
 #
 # Usage: droidducky.sh payload_file.dd
 #
+# Copyright (C) 2015 - Andrej Budinčević <andrew@hotmail.rs>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
